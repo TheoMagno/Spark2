3f023effd6cb1719ce1016fd845855fd6be0ff91
Theo Magno <theomagno@ua.pt>
2022-05-27 21:45:58 +0100

    Added part 4 and 5, and comments

--- src/main/scala/Spark2Application.scala
+++ src/main/scala/Spark2Application.scala
@@ -1,16 +1,17 @@
-import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
 import org.apache.spark.sql.{SparkSession, functions}
-import org.apache.spark.sql.functions.{array_distinct, col, collect_list, flatten, from_unixtime, regexp_extract, sort_array, unix_timestamp, when}
+import org.apache.spark.sql.functions.{array_distinct, avg, col, collect_list, count, explode, first, flatten, from_unixtime, lit, regexp_extract, sort_array, unix_timestamp}
 import org.apache.spark.sql.types.{DoubleType, IntegerType, LongType, StringType, StructField, StructType}
 
 object Spark2Application {
   def main(args: Array[String]) = {
+    //Create Spark session
     val spark = SparkSession.builder()
       .master("local")
       .appName("Spark 2 Recruitment Challenge")
       .getOrCreate()
 
     //Part 1
+    //Dataframe 1 Structure
     val schema_df_1 = StructType(
       StructField("App", StringType, true) ::
         StructField("Translated_Review", StringType, true) ::
@@ -18,15 +19,16 @@ object Spark2Application {
         StructField("Sentiment_Polarity", DoubleType, false) ::
         StructField("Sentiment_Subjectivity", DoubleType, false) :: Nil
     )
-
+    //Read csv file with schema
     var df = spark.read.options(Map("header"->"true", "escape"->"\"")).schema(schema_df_1).csv("src/main/resources/googleplaystore_user_reviews.csv").na.fill(0)
-
+    //Group by app name making average of sentiment polarity
     val df_1 = df.groupBy("App").avg("Sentiment_Polarity").withColumnRenamed("avg(Sentiment_Polarity)","Average_Sentiment_Polarity")
 
     df_1.printSchema()
 
     df_1.show()
     //Part 2
+    //Dataframe Structure to import the csv
     val schema_df_2 = StructType(
       StructField("App", StringType, false) ::
         StructField("Category", StringType, false) ::
@@ -42,18 +44,22 @@ object Spark2Application {
         StructField("Current Ver", StringType, false) ::
         StructField("Android Ver", StringType, false) :: Nil
     )
-
+    //Read csv file with schema
     df = spark.read.options(Map("header"->"true")).schema(schema_df_2).csv("src/main/resources/googleplaystore.csv").na.fill(0)
-
+    //Filter dataframe by showing only apps with Rating greater than 4.0, sorted in descending order
     val df_2 = df.filter(df("Rating")>4.0).sort(col("Rating").desc)
 
-    df_2.printSchema()
+    //Print schema to show that the structure is being used
+    //df_2.printSchema()
 
-    df_2.show()
+    //Show dataframe to check if is correct before writing
+    //df_2.show()
 
-    /*df_2.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header",
-      "true").options(Map("header"->"true", "delimiter"->"§")).csv("src/main/resoures/best_apps.csv")*/
+    //Write dataframe to csv file with delimiter §
+    df_2.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header",
+      "true").options(Map("header"->"true", "delimiter"->"§")).csv("src/main/resources/best_apps")
     //Part 3
+    //Dataframe structure to import the csv
     val schema_df_3 = StructType(
       StructField("App", StringType, false) ::
         StructField("Categories", StringType, false) ::
@@ -69,23 +75,42 @@ object Spark2Application {
         StructField("Current_Version", StringType, false) ::
         StructField("Minimum_Android_Version", StringType, false) :: Nil
     )
-
+    //Read csv using schema
     df = spark.read.options(Map("header"->"true")).schema(schema_df_3).csv("src/main/resources/googleplaystore.csv").na.fill(0)
-
+    //Temporary dataframe that changes Categories column to array of categories, groups by app name and aggregates using the maximum rating and the list of categories, removing duplicates
     val temp_1 =df.withColumn("Categories",  functions.split(col("Categories")," ").as("Categories"))
       .groupBy(col("App"))
-      .agg(flatten(collect_list(col("Categories"))).alias("Categories"), functions.max("Rating").as("Rating"))
+      .agg(array_distinct(flatten(collect_list(col("Categories")))).alias("Categories"), functions.max("Rating").as("Rating"))
+      .withColumn("Categories", array_distinct(sort_array(col("Categories"))))
 
+    //Temporary dataframe that changes the other fields (cast Size to Double, convert Price to euros, cast Last_Updated to Date, split Genres to Array of genres) and drops Categories and Rating
     val temp_2 = df.withColumn("Size",regexp_extract(col("Size"), "[0-9]+", 0).cast("Double"))
       .withColumn("Price",col("Price")*0.9)
       .withColumn("Last_Updated", from_unixtime(unix_timestamp(col("Last_Updated"), "MMMMM dd, yyyy")).cast("Date"))
       .withColumn("Genres", functions.split(col("Genres"),";").as("Genres"))
       .drop("Categories", "Rating")
+      .drop("Categories", "Rating")
 
+    //Join the two temporary dataframes, dropping duplicates, and produces Dataframe 3
     val df_3 = temp_1.join(temp_2, "App").dropDuplicates("App")
 
-    df_3.printSchema()
+    //Print schema to show that the structure is being used
+    //df_3.printSchema()
 
+    //Show dataframe to check if the dataframe is correct
     df_3.show()
+    //Part 4
+    //Join df_1 with df_3 using column App
+    val temp = df_3.join(df_1, "App")
+
+    //Write to parquet file with gzip compression
+    temp.write.mode("overwrite").options("compression"->"gzip", "header"->"true").parquet("src/main/resources/outputs/googleplaystore_cleaned")
+    //Part 5
+    val df_4 = temp.select(col("App"),explode(col("Genres")).alias("Genre")).join(temp, "App")
+      .groupBy("Genre").agg(count(lit(1)).alias("Count"), avg(col("Rating")).alias("Average_Rating"), avg(col("Average_Sentiment_Polarity")).alias("Average_Sentiment_Polarity"))
+
+    df_4.show()
+    //Write to parquet file with gzip compression
+    df_4.write.mode("overwrite").options("compression"->"gzip", "header"->"true").parquet("src/main/resources/outputs/googleplaystore_metrics")
   }
 }
